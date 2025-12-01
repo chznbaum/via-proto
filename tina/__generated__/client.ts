dc526658e34630d84ed4db2a4df06b3471aeb779
Chazona Baum <chazona@chazonabaum.com>
2025-11-30 20:00:59 -0500

    remove generated files from git

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/chazona/Repos/saas/via-proto/tina/__generated__/.cache/1764549924024', url: 'https://content.tinajs.io/1.6/content/e81a1638-e138-4fef-b1dc-656bbfc4ede5/github/main', token: '65b705e37fc5272ae3c8fae2b17adb32785f6873', queries,  });
-export default client;
-  
