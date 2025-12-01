5a6e7a2c34e9d3e504c22c1b892ee7b9bf710035
Chazona Baum <chazona@chazonabaum.com>
2025-12-01 08:24:44 -0500

    update tinylaunch badge

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -95,7 +95,7 @@ const Footer = () => {
                 className="mt-4 inline-block"
               >
                 <img
-                  src="https://cdn.viapro.to/tinylaunch_badge_launching_soon.svg"
+                  src="https://cdn.viapro.to/tinylaunch_badge_live_now.svg"
                   alt="TinyLaunch Badge"
                   width={202}
                   height={40}
